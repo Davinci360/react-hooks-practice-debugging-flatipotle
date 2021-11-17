ee9454f09ebb1d75e13eeba3ffd80689d8605cd7
davinci360 <jasonbrowntime@gmail.com>
2021-11-17 13:28:28 -0500

    2Hr mark, gathered info but app still not init

--- src/components/Form.js
+++ src/components/Form.js
@@ -13,7 +13,24 @@ const DEFAULT_STATE = {
 
 function Form() {
   const [formState, setFormState] = useState(DEFAULT_STATE);
+// <button onClick={() => this.handleClick()}>
+/*// Traditional Anonymous Function
+function (a){
+  return a + 100;
+}
 
+// Arrow Function Break Down
+
+// 1. Remove the word "function" and place arrow between the argument and opening body bracket
+(a) => {
+  return a + 100;
+}
+
+// 2. Remove the body braces and word "return" -- the return is implied.
+(a) => a + 100;
+
+// 3. Remove the argument parentheses
+a => a + 100;*/
   function handleSubmit() {
     event.preventDefault();
     props.addOrder(formState); // change to a =>
